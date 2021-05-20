f68e54c85e34189ea786f78e0351dd8c79e07d76
Jakob Håkansson <hakansson.jakob@gmail.com>
2021-05-20 14:03:51 +0200

    css grids added

---
+++ js/boende.js
@@ -0,0 +1,33 @@
+
+// Booking widget
+
+jQuery(document).ready(function() {
+	jQuery('#bookWidget-36690-64319-0-1621105362').bookWidget({
+		propid:64319,
+		alignment:'center',
+		availableColor:'#008000',
+		availableBackgroundColor:'#90ee90',
+		backgroundColor:'transparent',
+		borderColor:'transparent',
+		boxShadow:false,
+		buttonBackgroundColor:'#41b941',
+		buttonTitle:'Sök',
+		dateSelection:1,
+		defaultNightsAdvance:1,
+		defaultNumAdult:2,
+		defaultNumNight:2,
+		formAction:'https://beds24.com/booking.php',
+		formTarget:'_blank',
+		fontSize:'16px',
+		pastBackgroundColor:'#add8e6',
+		pastColor:'#869497',
+		peopleSelection:1,
+		showLabels:false,
+		unavailableColor:'#ff0000',
+		unavailableBackgroundColor:'#ffc0cb',
+		weekFirstDay:1,
+		widgetLang:'sv',
+		widgetType:'BookingStrip',
+		width:'960px'
+	});
+});
