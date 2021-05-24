accbf07e63ced584c3b19bd257aee5e974114658
Jakob Håkansson <hakansson.jakob@gmail.com>
2021-05-24 21:35:30 +0200

    bilder på plats

--- js/boende.js
+++ js/boende.js
@@ -31,3 +31,113 @@ jQuery(document).ready(function() {
 		width:'960px'
 	});
 });
+
+// Property
+
+function currentDiv1(n) {
+	showDivs1(slideIndex = n);
+  }
+  
+  function showDivs1(n) {
+	var i;
+	var x = document.getElementsByClassName("mySlides1");
+	var dots = document.getElementsByClassName("demo1");
+	if (n > x.length) {slideIndex = 1}
+	if (n < 1) {slideIndex = x.length}
+	for (i = 0; i < x.length; i++) {
+	  x[i].style.display = "none";
+	}
+	for (i = 0; i < dots.length; i++) {
+	  dots[i].className = dots[i].className.replace(" w3-opacity-off", "");
+	}
+	x[slideIndex-1].style.display = "block";
+	dots[slideIndex-1].className += " w3-opacity-off";
+  }
+
+// Studio small
+
+function currentDiv2(n) {
+	showDivs2(slideIndex = n);
+  }
+  
+  function showDivs2(n) {
+	var i;
+	var x = document.getElementsByClassName("mySlides2");
+	var dots = document.getElementsByClassName("demo2");
+	if (n > x.length) {slideIndex = 1}
+	if (n < 1) {slideIndex = x.length}
+	for (i = 0; i < x.length; i++) {
+	  x[i].style.display = "none";
+	}
+	for (i = 0; i < dots.length; i++) {
+	  dots[i].className = dots[i].className.replace(" w3-opacity-off", "");
+	}
+	x[slideIndex-1].style.display = "block";
+	dots[slideIndex-1].className += " w3-opacity-off";
+  }
+
+// Studio Standard
+
+function currentDiv3(n) {
+	showDivs3(slideIndex = n);
+  }
+  
+  function showDivs3(n) {
+	var i;
+	var x = document.getElementsByClassName("mySlides3");
+	var dots = document.getElementsByClassName("demo3");
+	if (n > x.length) {slideIndex = 1}
+	if (n < 1) {slideIndex = x.length}
+	for (i = 0; i < x.length; i++) {
+	  x[i].style.display = "none";
+	}
+	for (i = 0; i < dots.length; i++) {
+	  dots[i].className = dots[i].className.replace(" w3-opacity-off", "");
+	}
+	x[slideIndex-1].style.display = "block";
+	dots[slideIndex-1].className += " w3-opacity-off";
+  }
+
+// Studio Large
+
+function currentDiv4(n) {
+	showDivs4(slideIndex = n);
+  }
+  
+  function showDivs4(n) {
+	var i;
+	var x = document.getElementsByClassName("mySlides4");
+	var dots = document.getElementsByClassName("demo4");
+	if (n > x.length) {slideIndex = 1}
+	if (n < 1) {slideIndex = x.length}
+	for (i = 0; i < x.length; i++) {
+	  x[i].style.display = "none";
+	}
+	for (i = 0; i < dots.length; i++) {
+	  dots[i].className = dots[i].className.replace(" w3-opacity-off", "");
+	}
+	x[slideIndex-1].style.display = "block";
+	dots[slideIndex-1].className += " w3-opacity-off";
+  }
+
+// Studio Loft
+
+function currentDiv5(n) {
+	showDivs5(slideIndex = n);
+  }
+  
+  function showDivs5(n) {
+	var i;
+	var x = document.getElementsByClassName("mySlides5");
+	var dots = document.getElementsByClassName("demo5");
+	if (n > x.length) {slideIndex = 1}
+	if (n < 1) {slideIndex = x.length}
+	for (i = 0; i < x.length; i++) {
+	  x[i].style.display = "none";
+	}
+	for (i = 0; i < dots.length; i++) {
+	  dots[i].className = dots[i].className.replace(" w3-opacity-off", "");
+	}
+	x[slideIndex-1].style.display = "block";
+	dots[slideIndex-1].className += " w3-opacity-off";
+  }
